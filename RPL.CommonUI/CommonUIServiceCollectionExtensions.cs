8df30441f50f07fff6c19f5c80af70fc85f70358
shengjie_yan <ysjshengjie@live.cn>
2018-06-06 22:49:44 +0800

    Add project files.

---
+++ RPL.CommonUI/CommonUIServiceCollectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RPL.CommonUI
+{
+    public static class CommonUIServiceCollectionExtensions
+    {
+        public static void AddCommonUI(this IServiceCollection services)
+        {
+            services.ConfigureOptions(typeof(CommonUIConfigureOptions));
+        }
+    }
+}
